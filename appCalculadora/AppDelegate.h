b4414de342a772573d568b1386dac52e3ee97722
Marcos JRS <marcosjrs80@gmail.com>
2017-08-03 17:24:48 +0200

    Initial Commit

---
+++ appCalculadora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appCalculadora
+//
+//  Created by Marcos JRS on 3/8/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appCalculadora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appCalculadora
+//
+//  Created by Marcos JRS on 3/8/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
